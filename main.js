6db080a2c5eea2eca295b7921a8195e42b683f6f
Calixte <tchicoulo@hotmail.fr>
2018-06-18 15:26:42 +0200

    yolo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
   $.get("http://hp-api.herokuapp.com/api/characters").then(function(data) {
       data.forEach(function(personnages) {
       console.log(personnages);
-      $("body").append('<div class="card" style="width: 18rem;"><img class="card-img-top" src="'+ personnages.image +'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+ personnages.name +'</h5><p class="card-text">'+ personnages.house +'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>');
+      $("body").append('<div class="card" style="width: 18rem;"><img class="card-img-top" src="'+ personnages.image +'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+ personnages.name +'</h5><p class="card-text">'+ personnages.house +'</p></div></div>');
 });
 
 });
