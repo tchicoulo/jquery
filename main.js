7f837bb38b85f0eb66161b5815d9ca2053914047
Calixte <tchicoulo@hotmail.fr>
2018-06-18 14:57:25 +0200

    harry potter et ses amis

---
+++ main.js
@@ -0,0 +1,8 @@
+
+  $.get("http://hp-api.herokuapp.com/api/characters").then(function(data) {
+      data.forEach(function(personnages) {
+      console.log(personnages);
+      $("body").append('<div class="card" style="width: 18rem;"><img class="card-img-top" src="'+ personnages.image +'" alt="Card image cap"><div class="card-body"><h5 class="card-title">'+ personnages.name +'</h5><p class="card-text">'+ personnages.house +'</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>');
+});
+
+});
